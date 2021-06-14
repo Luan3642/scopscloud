ef2aa2082bf49c6f551883223de74d1f01e28d66
Matheus Souza <39911368+MatheusTsouza@users.noreply.github.com>
2021-06-13 22:26:05 -0300

    Add files via upload

--- update.php
+++ update.php
@@ -6,8 +6,10 @@ $data = file_get_contents('data.json');
 // decode json to array
 $json_arr = json_decode($data, true);
 
+$codigo = $_POST["codigo"]
+
 foreach ($json_arr as $key => $value) {
-    if ($value['codigo'] == $_POST["codigo"]) {
+    if ($value['codigo'] == $codigo) {
 		$json_arr[$key]['nome'] = $_POST["nome"];
         $json_arr[$key]['sobrenome'] = $_POST["sobrenome"];
 		$json_arr[$key]['email'] = $_POST["email"];
