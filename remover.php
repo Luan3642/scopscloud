aef60518b671ed4b1e8168b0f3053e2e2995964c
Luan Magalhães <luan3642@hotmail.com>
2021-06-13 23:22:35 -0300

    Update remover.php

--- remover.php
+++ remover.php
@@ -1,8 +1,8 @@
 <html>
 <body>
-<h1>Deletar</h1>
+<h1 style="text-align: center">Deletar</h1>
 <form action="delete.php" method="post">
-Codigo:<input type="text" name="codigo"><br>
+  <label style="font-weight: bold"> Codigo: </label><input type="text" name="codigo"><br>
 
 <input type="submit" href="index.php">
 </form>
