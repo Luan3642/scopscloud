ca187d8963550c400b7d0e6e200d89901fe05595
Matheus Souza <39911368+MatheusTsouza@users.noreply.github.com>
2021-06-13 22:14:13 -0300

    Add files via upload

--- atualizar.php
+++ atualizar.php
@@ -3,7 +3,9 @@
 <h1>Atualizar</h1>
 <form action="insert.php" method="post">
 Codigo:<input type="text" name="codigo"><br>
-
+Nome: <input type="text" name="nome"><br>
+Sobrenome: <input type="text" name="sobrenome"><br>
+Email: <input type="text" name="email"><br>
 <input type="submit" href="index.php">
 </form>
 
