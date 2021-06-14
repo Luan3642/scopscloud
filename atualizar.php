8ca8739fea9090ab4ea3279df58e42e132afe146
Luan Magalhães <luan3642@hotmail.com>
2021-06-13 23:25:07 -0300

    Update atualizar.php

--- atualizar.php
+++ atualizar.php
@@ -1,11 +1,12 @@
 <html>
 <body>
-<h1 style="text-align:center">Atualizar</h1>
+<h1 style="text-align:center; color: bold">Atualizar</h1>
 <form action="update.php" method="post">
   <label style="font-weight: bold">Codigo:</label><input type="text" name="codigo"><br>
   <label style="font-weight: bold"> Nome: </label> <input type="text" name="nome"><br>
   <label style="font-weight: bold"> Sobrenome: </label> <input type="text" name="sobrenome"><br>
   <label style="font-weight: bold"> Email: </label> <input type="text" name="email"><br>
+  <br>
 <input type="submit" href="index.php">
 </form>
 
